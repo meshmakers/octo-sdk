ded519e63f57697433b9e5e597551c2b80ab43af
Gerald Lochner <gerald.lochner@meshmakers.io>
2023-06-14 17:06:33 +0200

    Fix

--- src/sdk/PlugOperator/Models/PoolDescriptor.cs
+++
@@ -1,9 +0,0 @@
-namespace PlugOperator.Models;
-
-public class PoolDescriptor : K8Pool
-{
-    public string PlugControllerUri { get; set; } = string.Empty;
-    public string BrokerHost { get; set; } = string.Empty;
-    public string BrokerVirtualHost { get; set; } = string.Empty;
-    public int BrokerPort { get; set; } = 5672;
-}
