a4955a56d852eb805d1fac6fea3dec43df0cee9a
Gerald Lochner <gerald.lochner@meshmakers.io>
2023-12-30 13:27:47 +0100

    Added missing data transfer objects.

---
+++ src/Communication.Contracts/DataTransferObjects/SortOrdersDto.cs
@@ -0,0 +1,22 @@
+namespace Meshmakers.Octo.Communication.Contracts.DataTransferObjects;
+
+/// <summary>
+/// Order by direction.
+/// </summary>
+public enum SortOrdersDto
+{
+    /// <summary>
+    /// Default order.
+    /// </summary>
+    Default = 0,
+    
+    /// <summary>
+    /// Ascending order.
+    /// </summary>
+    Ascending = 1,
+    
+    /// <summary>
+    /// Descending order.
+    /// </summary>
+    Descending = 2
+}
