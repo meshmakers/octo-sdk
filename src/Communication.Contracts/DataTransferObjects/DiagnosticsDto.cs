1b87f96be286dc2625f692db680f0940d8d5f589
Gerald Lochner <gerald.lochner@meshmakers.io>
2023-12-30 16:00:08 +0100

    Added missing dto's

---
+++ src/Communication.Contracts/DataTransferObjects/DiagnosticsDto.cs
@@ -0,0 +1,33 @@
+using System.Security.Claims;
+
+// ReSharper disable UnusedAutoPropertyAccessor.Global
+
+// ReSharper disable MemberCanBePrivate.Global
+
+namespace Meshmakers.Octo.Communication.Contracts.DataTransferObjects;
+
+/// <summary>
+///     Data Transfer Object of diagnostics data
+/// </summary>
+public class DiagnosticsDto
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DiagnosticsDto"/> class.
+    /// </summary>
+    /// <param name="claimsPrincipal"></param>
+    public DiagnosticsDto(ClaimsPrincipal claimsPrincipal)
+    {
+        Name = claimsPrincipal.Identity?.Name;
+        Claims = claimsPrincipal.Claims.Select(x => new KeyValueDto(x));
+    }
+
+    /// <summary>
+    ///     Returns the identity name
+    /// </summary>
+    public string? Name { get; }
+
+    /// <summary>
+    ///     Returns claims
+    /// </summary>
+    public IEnumerable<KeyValueDto> Claims { get; }
+}
---
+++ src/Communication.Contracts/DataTransferObjects/KeyValueDto.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
+
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable UnusedAutoPropertyAccessor.Global
+
+namespace Meshmakers.Octo.Communication.Contracts.DataTransferObjects;
+
+/// <summary>
+///     Key Value data transfer object
+/// </summary>
+public class KeyValueDto
+{
+    /// <summary>
+    ///     Constructor
+    /// </summary>
+    /// <param name="claim">A security claim</param>
+    public KeyValueDto(Claim claim)
+    {
+        Key = claim.Type;
+        Value = claim.Value;
+    }
+
+    /// <summary>
+    ///     Constructor
+    /// </summary>
+    /// <param name="keyValuePair">A key value pair</param>
+    public KeyValueDto(KeyValuePair<string, string> keyValuePair)
+    {
+        Key = keyValuePair.Key;
+        Value = keyValuePair.Value;
+    }
+
+    /// <summary>
+    ///     Returns the key
+    /// </summary>
+    [Required]
+    public string Key { get; }
+
+    /// <summary>
+    ///     Return the value
+    /// </summary>
+    public string Value { get; }
+}
