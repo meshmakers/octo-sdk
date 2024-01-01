72790a92bac89d8655478ad48f1dfece44dcf3f8
Gerald Lochner <gerald.lochner@meshmakers.io>
2024-01-01 21:50:42 +0100

    Added definition for records.

---
+++ src/Communication.Contracts/DataTransferObjects/RtRecordDto.cs
@@ -0,0 +1,23 @@
+using System.Text.Json.Serialization;
+using Meshmakers.Octo.ConstructionKit.Contracts;
+
+namespace Meshmakers.Octo.Communication.Contracts.DataTransferObjects;
+
+/// <summary>
+/// Base class for all runtime records DTOs
+/// </summary>
+public class RtRecordDto : GraphQlDto
+{
+    /// <summary>
+    /// Gets or sets the type id of the entity
+    /// </summary>
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    public CkId<CkRecordId> CkRecordId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the properties of the entity
+    /// </summary>
+    [JsonExtensionData]
+    // ReSharper disable once UnusedAutoPropertyAccessor.Global
+    public IDictionary<string, object>? Properties { get; set; }
+}
