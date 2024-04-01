f1faad7574a2414447cde656b9987e7ba02dd3d2
Gerald Lochner <gerald.lochner@meshmakers.io>
2024-04-01 18:24:10 +0200

    AB#1088: New: Client for time series removed.

--- src/Sdk.ServiceClient/TimeSeries/ITimeSeriesServiceClientAccessToken.cs
+++
@@ -1,6 +0,0 @@
-namespace Meshmakers.Octo.Sdk.ServiceClient.TimeSeries;
-
-/// <summary>
-///     Represents the access token used to authenticate the client.
-/// </summary>
-public interface ITimeSeriesServiceClientAccessToken : IServiceClientAccessToken;
