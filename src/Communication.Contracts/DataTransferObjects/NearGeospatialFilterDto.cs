48f0f1cc99ac3472f39400581277eb885558bf43
Gerald Lochner <gerald.lochner@meshmakers.io>
2024-04-12 17:34:07 +0200

    AB#1221: New: Dto's for filter geo near a central point and result objects for queries containing geospatial data.

---
+++ src/Communication.Contracts/DataTransferObjects/NearGeospatialFilterDto.cs
@@ -0,0 +1,54 @@
+﻿using Meshmakers.Common.Shared;
+using Meshmakers.Octo.Runtime.Contracts.Geospatial.Geometry;
+
+namespace Meshmakers.Octo.Communication.Contracts.DataTransferObjects;
+
+/// <summary>
+/// Represents a filter value for a near field search
+/// </summary>
+public class NearGeospatialFilterDto : IGeospatialFilterDto
+{
+    /// <summary>
+    ///     Creates a new instance of <see cref="NearGeospatialFilterDto" />
+    /// </summary>
+    /// <param name="attributeName">The name of the attribute to compare</param>
+    /// <param name="point">Point to search for</param>
+    /// <param name="minDistance">The minimum distance from the center point that the documents can be.</param>
+    /// <param name="maxDistance">The maximum distance from the center point that the documents can be.</param>
+    public NearGeospatialFilterDto(string attributeName, Point point, double? minDistance, double? maxDistance)
+    {
+        ArgumentValidation.ValidateString(nameof(attributeName), attributeName);
+
+        AttributeName = attributeName;
+        Point = point;
+        MinDistance = minDistance;
+        MaxDistance = maxDistance;
+    }
+    
+    /// <summary>
+    /// Initializes a new instance of the <see cref="NearGeospatialFilterDto"/> class.
+    /// </summary>
+    /// <returns></returns>
+    public NearGeospatialFilterDto()
+    {
+      
+    }
+
+    /// <summary>
+    /// The point to search for
+    /// </summary>
+    public Point Point { get; set; } = null!;
+    
+    /// <summary>
+    /// The maximum distance from the center point that the documents can be.
+    /// </summary>
+    public double? MaxDistance { get; set;}
+    
+    /// <summary>
+    /// The minimum distance from the center point that the documents can be
+    /// </summary>
+    public double? MinDistance { get;  set;}
+
+    /// <inheritdoc />
+    public string AttributeName { get; set; } = null!;
+}
---
+++ src/Communication.Contracts/DataTransferObjects/RtGeospatialValueDto.cs
@@ -0,0 +1,19 @@
+﻿using Meshmakers.Octo.Runtime.Contracts.Geospatial.Geometry;
+
+namespace Meshmakers.Octo.Communication.Contracts.DataTransferObjects;
+
+/// <summary>
+/// Represents a geospatial value of  
+/// </summary>
+public class RtGeospatialValueDto
+{
+    /// <summary>
+    /// The geographical distance if a geospatial query has been applied
+    /// </summary>
+    public double? Distance { get; set; }
+
+    /// <summary>
+    /// Gets the geospatial point
+    /// </summary>
+    public Point Point { get; set; } = null!;
+}
