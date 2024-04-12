d3862f59dd3bd9c90ee00a39dd2530f7634777c9
Gerald Lochner <gerald.lochner@meshmakers.io>
2024-04-12 17:34:07 +0200

    AB#1221: New: Dto's for filter geo near a central point and result objects for queries containing geospatial data.

---
+++ src/Communication.Contracts/DataTransferObjects/IGeospatialFilterDto.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Meshmakers.Octo.Communication.Contracts.DataTransferObjects;
+
+/// <summary>
+/// Interface for a geospatial filter
+/// </summary>
+public interface IGeospatialFilterDto
+{
+    /// <summary>
+    /// The attribute name to search for
+    /// </summary>
+    public string AttributeName { get; set; }
+}
