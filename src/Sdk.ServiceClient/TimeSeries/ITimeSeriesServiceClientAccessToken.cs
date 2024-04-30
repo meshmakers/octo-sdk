bdb8d49805eb88d5896a25d36cba26403e757ff4
Reimar Klammer <reimar.klammer@meshmakers.io>
2024-04-30 13:19:15 +0200

    Revert "AB#1088: New: Client for time series removed."
    This reverts commit f1faad7574a2414447cde656b9987e7ba02dd3d2.

---
+++ src/Sdk.ServiceClient/TimeSeries/ITimeSeriesServiceClientAccessToken.cs
@@ -0,0 +1,6 @@
+namespace Meshmakers.Octo.Sdk.ServiceClient.TimeSeries;
+
+/// <summary>
+///     Represents the access token used to authenticate the client.
+/// </summary>
+public interface ITimeSeriesServiceClientAccessToken : IServiceClientAccessToken;
---
+++ src/Sdk.ServiceClient/TimeSeries/TimeSeriesServiceClientOptions.cs
@@ -0,0 +1,6 @@
+namespace Meshmakers.Octo.Sdk.ServiceClient.TimeSeries;
+
+/// <summary>
+///     Options for configuration of the TimeSeries services client proxy.
+/// </summary>
+public class TimeSeriesServiceClientOptions : ServiceClientOptions;
