28585799a38eb3fc09919f19a57682a177b940a9
Reimar Klammer <reimar.klammer@meshmakers.io>
2024-06-18 15:09:33 +0200

    AB#1077: Add another place where to pass through data

--- src/Sdk.Common/Services/ExecutePipelineOptions.cs
+++ src/Sdk.Common/Services/ExecutePipelineOptions.cs
@@ -3,7 +3,7 @@ namespace Meshmakers.Octo.Sdk.Common.Services;
 /// <summary>
 /// Options for executing a pipeline
 /// </summary>
-public class ExecutePipelineOptions(DateTime transactionStartedDateTime, IDictionary<string, object?>? properties = null)
+public class ExecutePipelineOptions(DateTime transactionStartedDateTime)
 {
     /// <summary>
     /// Gets or sets the date and time when the transaction started
@@ -14,9 +14,4 @@ public class ExecutePipelineOptions(DateTime transactionStartedDateTime, IDictio
     /// Gets or sets the date and time when the transaction started
     /// </summary>
     public DateTime? ExternalReceivedDateTime { get; set; }
-
-    /// <summary>
-    /// Context properties that can be used to share data between the adapter and the pipeline
-    /// </summary>
-    public IDictionary<string, object?>? Properties { get; } = properties;
 }
