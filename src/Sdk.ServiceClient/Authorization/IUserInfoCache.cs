babc984dfb68b8a09957ebcb870e5a3e2ba5c704
meshmakers <request@meshmakers.io>
2024-07-29 20:33:14 +0200

    AB#1426: New: Introduced IUserInfoCache to cache user info data for short time to reduce the amounts of calls to identity server.

---
+++ src/Sdk.ServiceClient/Authorization/IUserInfoCache.cs
@@ -0,0 +1,14 @@
+﻿namespace Meshmakers.Octo.Sdk.ServiceClient.Authorization;
+
+/// <summary>
+/// Interface to access optionally cached user info from the authorization server.
+/// </summary>
+public interface IUserInfoCache
+{
+    /// <summary>
+    ///     Get (cached) user info from the authorization server.
+    /// </summary>
+    /// <param name="accessToken">The access token to use for the user info request.</param>
+    /// <returns></returns>
+    Task<UserInfoData> GetUserInfoAsync(string accessToken);
+}
