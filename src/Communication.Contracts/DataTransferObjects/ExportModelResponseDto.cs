818f8f4cc768489fded3958a5e298294f018b2d9
Gerald Lochner <gerald.lochner@meshmakers.io>
2024-08-05 09:00:53 +0200

    AB#1434: Fix: Reworked error handling and request and response dtos for version 3

--- src/Communication.Contracts/DataTransferObjects/ExportModelResponseDto.cs
+++ src/Communication.Contracts/DataTransferObjects/ExportModelResponseDto.cs
@@ -6,8 +6,17 @@ namespace Meshmakers.Octo.Communication.Contracts.DataTransferObjects;
 public class ExportModelResponseDto
 {
     /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="jobId"></param>
+    public ExportModelResponseDto(string jobId)
+    {
+        JobId = jobId;
+    }
+
+    /// <summary>
     ///     Gets or sets the job id.
     /// </summary>
     // ReSharper disable once UnusedAutoPropertyAccessor.Global
-    public string? JobId { get; set; }
+    public string JobId { get; set; }
 }
