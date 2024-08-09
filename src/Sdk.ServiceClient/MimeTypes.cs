355e2f86060938df39cecbc39b41561d98bd4e2f
Gerald Lochner <gerald.lochner@meshmakers.io>
2024-08-09 14:31:13 +0200

    AB#1441: Fix: Mime types centralized

---
+++ src/Sdk.ServiceClient/MimeTypes.cs
@@ -0,0 +1,22 @@
+namespace Meshmakers.Octo.Sdk.ServiceClient;
+
+/// <summary>
+/// Used MIME types in Octo Mesh
+/// </summary>
+public static class MimeTypes
+{
+    /// <summary>
+    /// JSON MIME type
+    /// </summary>
+    public const string MimeTypeJson = "application/json";
+    
+    /// <summary>
+    /// ZIP MIME type
+    /// </summary>
+    public const string MimeTypeZip = "application/zip";
+    
+    /// <summary>
+    /// YAML MIME type
+    /// </summary>
+    public const string MimeTypeYaml = "application/x-yaml";
+}
