7abc4e73cbcaeaa3b49c09d8b16649aaef088c57
Reimar Klammer <reimar.klammer@meshmakers.io>
2024-08-26 08:42:31 +0200

    Small bugfixes and improvements regarding metadata

--- src/Sdk.Common/EtlDataPipeline/Nodes/Buffering/BufferSchedulerHostedService.cs
+++ src/Sdk.Common/EtlDataPipeline/Nodes/Buffering/BufferSchedulerHostedService.cs
@@ -13,16 +13,12 @@ internal interface IBufferScheduler
 internal class BufferScheduler(ILogger<BufferScheduler> logger) : IBufferScheduler
 {
     private bool _shouldStop;
-    private readonly Stack<ExecutionItem> _tasks = [];
+    private readonly Queue<ExecutionItem> _tasks = [];
     
     public void ScheduleOrReplace(Func<Task> action, TimeSpan delay)
     {
-        if (_tasks.Count != 0)
-        {
-            _ = _tasks.Pop();
-        }
-
-        _tasks.Push(new ExecutionItem(action, delay));
+        logger.LogInformation("Scheduling new task. Delay: {Delay}. Currently {TaskCount} tasks in queue.", delay, _tasks.Count);
+        _tasks.Enqueue(new ExecutionItem(action, delay));
     }
     
     public Task StartAsync(CancellationToken cancellationToken)
@@ -44,16 +40,27 @@ internal class BufferScheduler(ILogger<BufferScheduler> logger) : IBufferSchedul
         _shouldStop = false;
         while (!_shouldStop && !token.IsCancellationRequested)
         {
-            var tasksToRun = _tasks.Where(task => DateTimeOffset.UtcNow - task.LastExecution >= task.Delay).ToList();
-            
-            logger.LogDebug("Found {Count} tasks to run", tasksToRun.Count);
-            
-            foreach (var task in tasksToRun)
+            // we only want to run the oldest task and remove it if its not the last one.
+            if (_tasks.TryPeek(out var task) && DateTimeOffset.UtcNow - task!.LastExecution >= task.Delay)
             {
-                task.Action().ConfigureAwait(false).GetAwaiter().GetResult();
-                task.LastExecution = DateTimeOffset.UtcNow;
-            }
+                try
+                {
+                    logger.LogInformation("Running buffer retrieval task.");
+                    task.Action().ConfigureAwait(false).GetAwaiter().GetResult();
+                    task.LastExecution = DateTimeOffset.UtcNow;
 
+                    if (_tasks.Count > 1)
+                    {
+                        logger.LogInformation("Ran task the last time. Removing it.");
+                        _ = _tasks.Dequeue();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, "Failed to run task.");
+                }
+            }
+            
             Thread.Sleep(1_000);
         }
     }
@@ -86,13 +93,26 @@ internal class BufferSchedulerHostedService : IHostedService
 
     public Task StartAsync(CancellationToken cancellationToken)
     {
-        _scheduler.StartAsync(cancellationToken);
-        return Task.CompletedTask;
+        return _scheduler.StartAsync(cancellationToken);
     }
 
     public Task StopAsync(CancellationToken cancellationToken)
     {
-        _scheduler.StopAsync();
-        return Task.CompletedTask;
+        return _scheduler.StopAsync();
+    }
+}
+
+internal static class QueueExtensions
+{
+    public static bool TryPeek<T>(this Queue<T> queue, out T? result)
+    {
+        if (queue.Count == 0)
+        {
+            result = default;
+            return false;
+        }
+
+        result = queue.Peek();
+        return true;
     }
 }
