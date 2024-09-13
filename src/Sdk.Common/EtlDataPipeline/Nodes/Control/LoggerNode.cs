5207dac1c6d46a467cbae84714be41e1e5eefe2a
Gerald Lochner <gerald.lochner@meshmakers.io>
2024-09-13 09:53:02 +0200

    AB#1463: New: Moved logger node from mesh adapter

---
+++ src/Sdk.Common/EtlDataPipeline/Nodes/Control/LoggerNode.cs
@@ -0,0 +1,31 @@
+using Meshmakers.Octo.Sdk.Common.EtlDataPipeline.Configuration;
+
+namespace Meshmakers.Octo.Sdk.Common.EtlDataPipeline.Nodes.Control;
+
+/// <summary>
+/// Configuration for node logger
+/// </summary>
+[NodeName("Logger", 1)]
+public class LoggerNodeConfiguration : NodeConfiguration
+{
+    /// <summary>
+    /// Message to log
+    /// </summary>
+    public string Message { get; init; } = null!;
+}
+
+/// <summary>
+/// Logs a message
+/// </summary>
+/// <param name="next"></param>
+[NodeConfiguration(typeof(LoggerNodeConfiguration))]
+internal class LoggerNode(NodeDelegate next) : IPipelineNode
+{
+    public async Task ProcessObjectAsync(IDataContext dataContext)
+    {
+        var c = dataContext.GetNodeConfiguration<LoggerNodeConfiguration>();
+        dataContext.Logger.Info(dataContext.NodeStack.Peek(), c.Message);
+
+        await next(dataContext);
+    }
+}
