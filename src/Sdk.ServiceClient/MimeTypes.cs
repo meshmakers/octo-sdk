9530bd7257d8156cbcb08a89ef480babfac8a1b9
Gerald Lochner <gerald.lochner@meshmakers.io>
2024-09-18 14:33:43 +0200

    AB#1455: New: Added more mime types specific to Windows.

--- src/Sdk.ServiceClient/MimeTypes.cs
+++ src/Sdk.ServiceClient/MimeTypes.cs
@@ -14,9 +14,19 @@ public static class MimeTypes
     /// ZIP MIME type
     /// </summary>
     public const string MimeTypeZip = "application/zip";
+
+    /// <summary>
+    /// Zip a compressed MIME type
+    /// </summary>
+    public const string MimeTypeXZipCompressed = "application/x-zip-compressed";
     
     /// <summary>
     /// YAML MIME type
     /// </summary>
     public const string MimeTypeYaml = "application/x-yaml";
+
+    /// <summary>
+    /// Unkown file type - currently used by windows when uploading YAML files
+    /// </summary>
+	public const string Unknown = "application/octet-stream";
 }
