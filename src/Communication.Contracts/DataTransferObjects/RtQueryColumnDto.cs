a551f39f89b74d1e591a388d5d0fb88d267f7041
Gerald Lochner <gerald.lochner@meshmakers.io>
2024-11-11 09:39:10 +0100

    AB#1887: New: Added definitions for runtime query and column for graphql

---
+++ src/Communication.Contracts/DataTransferObjects/RtQueryColumnDto.cs
@@ -0,0 +1,22 @@
+using System.Text.Json.Serialization;
+using Meshmakers.Octo.ConstructionKit.Contracts.DataTransferObjects;
+
+namespace Meshmakers.Octo.Communication.Contracts.DataTransferObjects;
+
+/// <summary>
+/// Represents a query column of a runtime query
+/// </summary>
+// ReSharper disable once ClassNeverInstantiated.Global
+public class RtQueryColumnDto : GraphQlDto
+{
+    /// <summary>
+    ///     Gets or sets the attribute path.
+    /// </summary>
+    public required string AttributePath { get; set; }
+    
+    /// <summary>
+    ///     Gets or sets the attribute data type.
+    /// </summary>
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public required AttributeValueTypesDto AttributeValueType { get; set; }
+}
