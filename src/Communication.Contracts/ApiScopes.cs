8df306f132131f9a44a5313237b48ae1c652cdd2
Gerald Lochner <gerald.lochner@meshmakers.io>
2025-04-19 14:12:36 +0200

    AB#1915: New: Rework API of asset repository

--- src/Communication.Contracts/ApiScopes.cs
+++ src/Communication.Contracts/ApiScopes.cs
@@ -20,54 +20,64 @@ public enum ApiScopes
     /// Asset repository API readonly access
     /// </summary>
     AssetSystemApiReadOnly = 2,
+
+    /// <summary>
+    /// Asset repository API full access
+    /// </summary>
+    AssetTenantApiFullAccess = 4,
+
+    /// <summary>
+    /// Asset repository API readonly access
+    /// </summary>
+    AssetTenantApiReadOnly = 8,
     
     /// <summary>
     /// Identity API full access
     /// </summary>
-    IdentityApiFullAccess = 4,
+    IdentityApiFullAccess = 16,
     
     /// <summary>
     /// Identity API readonly access
     /// </summary>
-    IdentityApiReadOnly = 8,
+    IdentityApiReadOnly = 32,
     
     /// <summary>
     /// Bot API full access
     /// </summary>
-    BotApiFullAccess = 16,
+    BotApiFullAccess = 64,
     
     /// <summary>
     /// Bot API readonly access
     /// </summary>
-    BotApiReadOnly = 32,
+    BotApiReadOnly = 128,
     
     /// <summary>
     /// Communication service system API full access
     /// </summary>
-    CommunicationServiceSystemApiFullAccess = 64,
+    CommunicationServiceSystemApiFullAccess = 256,
     
     /// <summary>
     /// Communication service tenant API full access
     /// </summary>
-    CommunicationServiceTenantApiFullAccess = 128,
+    CommunicationServiceTenantApiFullAccess = 512,
     
     /// <summary>
     /// Communication service tenant API readonly access
     /// </summary>
-    CommunicationServiceTenantApiReadOnly = 256,
+    CommunicationServiceTenantApiReadOnly = 1024,
 
     /// <summary>
     /// Reporting service system API full access
     /// </summary>
-    ReportingServiceSystemApiFullAccess = 512,
+    ReportingServiceSystemApiFullAccess = 2048,
 
     /// <summary>
     /// Reporting service tenant API readonly access
     /// </summary>
-    ReportingServiceTenantApiFullAccess = 1024,
+    ReportingServiceTenantApiFullAccess = 4096,
 
     /// <summary>
     /// Reporting service tenant API readonly access
     /// </summary>
-    ReportingServiceTenantApiReadOnly = 2048,
+    ReportingServiceTenantApiReadOnly = 8096,
 }
