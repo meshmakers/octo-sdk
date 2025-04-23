5fb19f82c92befb1a30d9b1b55f2fcabfbeb631f
Gerald Lochner <gerald.lochner@meshmakers.io>
2025-04-23 15:46:12 +0200

    AB#1915: New: Added result for report generation endpoint

---
+++ src/Communication.Contracts/DataTransferObjects/ReportStoreResultDto.cs
@@ -0,0 +1,12 @@
+namespace Meshmakers.Octo.Communication.Contracts.DataTransferObjects;
+
+/// <summary>
+/// Represents the result of a report generation request.
+/// </summary>
+public class ReportStoreResultDto
+{
+    /// <summary>
+    /// Returns the uri of the generated report
+    /// </summary>
+    public string ReportUri { get; set; } = string.Empty;
+}
