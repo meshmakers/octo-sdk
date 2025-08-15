0ea4b954689a3f30980f0a30fc835fe142ffaf47
Gerald Lochner <gerald.lochner@meshmakers.io>
2025-08-15 13:08:47 +0200

    AB#2457: New: Make source generator for DTO's for GraphQL more complete

--- src/Sdk.SourceGeneration/AttributeCodeGenerator.cs
+++ src/Sdk.SourceGeneration/AttributeCodeGenerator.cs
@@ -8,7 +8,7 @@ namespace Meshmakers.Octo.Sdk.SourceGeneration;
 internal static class AttributeCodeGenerator
 {
     internal static void GenerateNullableProperty(CkTypeAttributeDto ckTypeAttributeDto, StringBuilder sb,
-        CkAttributeGraph ckAttributeGraph)
+        CkAttributeGraph ckAttributeGraph, bool isMutation)
     {
         switch (ckAttributeGraph.ValueType)
         {
@@ -72,148 +72,42 @@ internal static class AttributeCodeGenerator
 
                 break;
             case AttributeValueTypesDto.Record:
-                // Not supported by the generator
-                // if (ckAttributeGraph.ValueCkRecordId != null)
-                // {
-                //     sb.AppendLine(
-                //         $"  public Rt{ckAttributeGraph.ValueCkRecordId.Key.RecordId.MakeClassName()}Record? {ckTypeAttributeDto.AttributeName}");
-                //     sb.AppendLine("  {");
-                //     sb.AppendLine("      get; set;");
-                //     sb.AppendLine("  }");
-                // }
-
-                break;
-            case AttributeValueTypesDto.StringArray:
-                sb.AppendLine($"  public IAttributeValueList<string>? {ckTypeAttributeDto.AttributeName}");
-                sb.AppendLine("  {");
-                sb.AppendLine("      get; set;");
-                sb.AppendLine("  }");
-                break;
-            case AttributeValueTypesDto.IntArray:
-                // Not supported by the generator
-                // sb.AppendLine($"  public IAttributeValueList<long>? {ckTypeAttributeDto.AttributeName}");
-                // sb.AppendLine("  {");
-                // sb.AppendLine("      get; set;");
-                // sb.AppendLine("  }");
-                break;
-            case AttributeValueTypesDto.RecordArray:
-                // Not supported by the generator
-                // if (ckAttributeGraph.ValueCkRecordId != null)
-                // {
-                //     sb.AppendLine(
-                //         $"  public IAttributeValueList<Rt{ckAttributeGraph.ValueCkRecordId.Key.RecordId.MakeClassName()}Record>? {ckTypeAttributeDto.AttributeName}");
-                //     sb.AppendLine("  {");
-                //     sb.AppendLine("      get; set;");
-                //     sb.AppendLine("  }");
-                // }
-
-                break;
-            default:
-                sb.AppendLine($"  // Unsupported by Generator: {ckTypeAttributeDto.AttributeName} (Type: {ckAttributeGraph.ValueType})");
-                break;
-        }
-    }
-
-    internal static void GenerateNonNullableProperty(CkTypeAttributeDto ckTypeAttributeDto, StringBuilder sb,
-        CkAttributeGraph ckAttributeGraph)
-    {
-        switch (ckAttributeGraph.ValueType)
-        {
-            case AttributeValueTypesDto.String:
-                sb.AppendLine($"  public required string {ckTypeAttributeDto.AttributeName}");
-                sb.AppendLine("  {");
-                sb.AppendLine("      get; set;");
-                sb.AppendLine("  }");
-                break;
-            case AttributeValueTypesDto.Int:
-                sb.AppendLine($"  public int {ckTypeAttributeDto.AttributeName}");
-                sb.AppendLine("  {");
-                sb.AppendLine("      get; set;");
-                sb.AppendLine("  }");
-                break;
-            case AttributeValueTypesDto.Int64:
-                sb.AppendLine($"  public long {ckTypeAttributeDto.AttributeName}");
-                sb.AppendLine("  {");
-                sb.AppendLine("      get; set;");
-                sb.AppendLine("  }");
-                break;
-            case AttributeValueTypesDto.DateTime:
-                sb.AppendLine($"  public global::System.DateTime {ckTypeAttributeDto.AttributeName}");
-                sb.AppendLine("  {");
-                sb.AppendLine("      get; set;");
-                sb.AppendLine("  }");
-                break;
-            case AttributeValueTypesDto.DateTimeOffset:
-                sb.AppendLine($"  public global::System.DateTimeOffset {ckTypeAttributeDto.AttributeName}");
-                sb.AppendLine("  {");
-                sb.AppendLine("      get; set;");
-                sb.AppendLine("  }");
-                break;
-            case AttributeValueTypesDto.TimeSpan:
-                sb.AppendLine($"  public global::System.TimeSpan {ckTypeAttributeDto.AttributeName}");
-                sb.AppendLine("  {");
-                sb.AppendLine("      get; set;");
-                sb.AppendLine("  }");
-                break;
-            case AttributeValueTypesDto.Double:
-                sb.AppendLine($"  public double {ckTypeAttributeDto.AttributeName}");
-                sb.AppendLine("  {");
-                sb.AppendLine("      get; set;");
-                sb.AppendLine("  }");
-                break;
-            case AttributeValueTypesDto.Boolean:
-                sb.AppendLine($"  public bool {ckTypeAttributeDto.AttributeName}");
-                sb.AppendLine("  {");
-                sb.AppendLine("      get; set;");
-                sb.AppendLine("  }");
-                break;
-            case AttributeValueTypesDto.Enum:
-                if (ckAttributeGraph.ValueCkEnumId != null)
+                if (ckAttributeGraph.ValueCkRecordId != null)
                 {
                     sb.AppendLine(
-                        $"  public Rt{ckAttributeGraph.ValueCkEnumId.Key.EnumId.MakeClassName()}Enum {ckTypeAttributeDto.AttributeName}");
+                        isMutation
+                            ? $"  public Rt{ckAttributeGraph.ValueCkRecordId.Key.RecordId.MakeClassName()}RecordMutationDto? {ckTypeAttributeDto.AttributeName}"
+                            : $"  public Rt{ckAttributeGraph.ValueCkRecordId.Key.RecordId.MakeClassName()}RecordDto? {ckTypeAttributeDto.AttributeName}");
                     sb.AppendLine("  {");
                     sb.AppendLine("      get; set;");
                     sb.AppendLine("  }");
                 }
-
-                break;
-            case AttributeValueTypesDto.Record:
-                // Not supported by the generator
-                // if (ckAttributeGraph.ValueCkRecordId != null)
-                // {
-                //     sb.AppendLine(
-                //         $"  public Rt{ckAttributeGraph.ValueCkRecordId.Key.RecordId.MakeClassName()}Record {ckTypeAttributeDto.AttributeName}");
-                //     sb.AppendLine("  {");
-                //     sb.AppendLine("      get; set;");
-                //     sb.AppendLine("  }");
-                // }
-
                 break;
             case AttributeValueTypesDto.StringArray:
-                // Not supported by the generator
-                // sb.AppendLine($"  public IAttributeValueList<string> {ckTypeAttributeDto.AttributeName}");
-                // sb.AppendLine("  {");
-                // sb.AppendLine("      get; set;");
-                // sb.AppendLine("  }");
+                sb.AppendLine($"  public IEnumerable<string>? {ckTypeAttributeDto.AttributeName}");
+                sb.AppendLine("  {");
+                sb.AppendLine("      get; set;");
+                sb.AppendLine("  }");
                 break;
             case AttributeValueTypesDto.IntArray:
-                // Not supported by the generator
-                // sb.AppendLine($"  public IAttributeValueList<long> {ckTypeAttributeDto.AttributeName}");
-                // sb.AppendLine("  {");
-                // sb.AppendLine("      get; set;");
-                // sb.AppendLine("  }");
+                sb.AppendLine($"  public IEnumerable<int>? {ckTypeAttributeDto.AttributeName}");
+                sb.AppendLine("  {");
+                sb.AppendLine("      get; set;");
+                sb.AppendLine("  }");
                 break;
             case AttributeValueTypesDto.RecordArray:
                 // Not supported by the generator
-                // if (ckAttributeGraph.ValueCkRecordId != null)
-                // {
-                //     sb.AppendLine(
-                //         $"  public IAttributeValueList<Rt{ckAttributeGraph.ValueCkRecordId.Key.RecordId.MakeClassName()}Record> {ckTypeAttributeDto.AttributeName}");
-                //     sb.AppendLine("  {");
-                //     sb.AppendLine("      get; set;");
-                //     sb.AppendLine("  }");
-                // }
+                if (ckAttributeGraph.ValueCkRecordId != null)
+                {
+                    sb.AppendLine(
+                        isMutation
+                            ? $"  public IEnumerable<Rt{ckAttributeGraph.ValueCkRecordId.Key.RecordId.MakeClassName()}RecordMutationDto>? {ckTypeAttributeDto.AttributeName}"
+                            : $"  public IEnumerable<Rt{ckAttributeGraph.ValueCkRecordId.Key.RecordId.MakeClassName()}RecordDto>? {ckTypeAttributeDto.AttributeName}");
+                    sb.AppendLine("  {");
+                    sb.AppendLine("      get; set;");
+                    sb.AppendLine("  }");
+                }
+                sb.AppendLine($"  // Unsupported by Generator: {ckTypeAttributeDto.AttributeName} (Type: {ckAttributeGraph.ValueType})");
 
                 break;
             default:
---
+++ src/Sdk.SourceGeneration/IMutationDtoCodeGenerator.cs
@@ -0,0 +1,31 @@
+using Meshmakers.Octo.ConstructionKit.Contracts;
+using Meshmakers.Octo.ConstructionKit.Contracts.DataTransferObjects;
+using Meshmakers.Octo.ConstructionKit.Contracts.Services;
+
+namespace Meshmakers.Octo.Sdk.SourceGeneration;
+
+/// <summary>
+///     Interface for generates code for mutation DTO's
+/// </summary>
+public interface IMutationDtoCodeGenerator : IEquatable<IMutationDtoCodeGenerator>
+{
+   /// <summary>
+   /// Generates code for a type for GraphQL mutations
+   /// </summary>
+   /// <param name="ns">The namespace of the created code</param>
+   /// <param name="ckType">Construction kit type instance</param>
+   /// <param name="cacheTenantId">Tenant id to access the correct cache instance</param>
+   /// <param name="cacheService">The cache service</param>
+   /// <returns></returns>
+   string GenerateType(string ns, CkTypeDto ckType, string cacheTenantId, ICkCacheService cacheService);
+
+   /// <summary>
+   /// Generates code for a record for GraphQL mutations
+   /// </summary>
+   /// <param name="ns">The namespace of the created code</param>
+   /// <param name="ckRecord">CkRecordDto instance</param>
+   /// <param name="cacheTenantId">Tenant id to access the correct cache instance</param>
+   /// <param name="cacheService">The cache service</param>
+   /// <returns></returns>
+   string GenerateRecord(string ns, CkRecordDto ckRecord, string cacheTenantId, ICkCacheService cacheService);
+}
