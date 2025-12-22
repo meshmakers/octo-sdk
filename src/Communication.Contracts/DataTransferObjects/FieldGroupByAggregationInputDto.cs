33fe79e920dcd6bb25cf3ea1a0d648ff36d7c7f7
Reimar Klammer <reimar.klammer@meshmakers.io>
2025-12-22 13:43:40 +0100

    AB#3085 feat: default resolveEnumValuesToNames to true for aggregations

--- src/Communication.Contracts/DataTransferObjects/FieldGroupByAggregationInputDto.cs
+++ src/Communication.Contracts/DataTransferObjects/FieldGroupByAggregationInputDto.cs
@@ -9,4 +9,10 @@ public class FieldGroupByAggregationInputDto : AggregationInputDto
     ///     Attribute paths to group by
     /// </summary>
     public IEnumerable<string> GroupByAttributePaths { get; set; } = null!;
+
+    /// <summary>
+    ///     If true, resolve enum integer values to their label names in groupBy keys.
+    ///     Defaults to true.
+    /// </summary>
+    public bool ResolveEnumValuesToNames { get; set; } = true;
 }
